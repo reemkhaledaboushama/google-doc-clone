4fd0910883f403f2e1c1ae44ba09d036a2663b13
Reem Khaled <reemkhaledaboushama@gmail.com>
2022-06-26 13:09:02 +0200

    aa

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("http://localhost:3001")
+    const s = io(process.env.REACT_APP_SERVER)
     setSocket(s)
 
     return () => {

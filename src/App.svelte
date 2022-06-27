10a4fc1d8c159dc2c93fa7b36fba397fbd1951ac
Mariam Meckawy <69309651+Mmeckawy@users.noreply.github.com>
2022-06-27 02:44:55 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,19 @@
   import Header from "./components/Header.svelte";
   import SideNav from "./components/SideNav.svelte";
   import CodeEditor from "./components/CodeEditor.svelte";
-  import { onMount } from "svelte";
-
+  
+  import {posts, timeout} from "./stores.js"
+ onMount(async () => {
+   if($posts.length<1 || $timeout == true){
+     const res = await fetch(apiBaseUrl + "/documents");
+     $posts = await res.json();
+   }
+});
 </script>
+
+  {#each posts as post}
+    <h5>{post.title}</h5>
+  {/each}
 
 <Header imageSrc="images/LogoMakr-38S17p.png" />
 <div class="wrapper">
